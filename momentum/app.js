5a57208e0c6e28ea02b46ce29530ca2e809d1904
reumna <91860050+reumna@users.noreply.github.com>
2021-10-21 14:30:09 +0900

    practice

---
+++ momentum/app.js
@@ -0,0 +1,20 @@
+const calculator = {
+    plus : function (a,b) {
+        console.log(a+b);
+    },
+    minus : function (a,b){
+        console.log(a-b);
+    },
+    time : function (a,b) {
+        console.log(a**b);
+    },
+    divide :function (a,b) {
+        console.log(a/b);
+    }
+    
+}
+
+calculator.plus(6,2)
+calculator.minus(6,2)
+calculator.time(6,2)
+calculator.divide(6,2)
